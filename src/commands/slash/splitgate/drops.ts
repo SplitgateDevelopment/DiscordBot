331cc9bef488bf97a375d7c18bb8e3aec866b21f
PADO <62028267+47PADO47@users.noreply.github.com>
2023-01-01 19:52:20 +0100

    ♻️ Update drops.ts slashCommand to new structure

--- src/commands/slash/splitgate/drops.ts
+++ src/commands/slash/splitgate/drops.ts
@@ -1,18 +1,24 @@
-import { EmbedBuilder } from 'discord.js';
+import { CommandInteraction, EmbedBuilder } from 'discord.js';
+import Bot from '../../../Bot';
 import SlashCommand from '../../../util/structures/SlashCommand';
 
-export default new SlashCommand({
-    name: 'drops',
-    description: 'Retrieve user\'s drops count',
-    options: [
-        {
-            name: 'userid',
-            description: 'Splitgate user identifier',
-            type: 3,
-            required: true
-        }
-    ],
-    run: async (client, interaction) => {
+class DropsCommand extends SlashCommand {
+    constructor() {
+        super({
+            name: 'drops',
+            description: 'Retrieve user\'s drops count',
+            options: [
+                {
+                    name: 'userid',
+                    description: 'Splitgate user identifier',
+                    type: 3,
+                    required: true
+                }
+            ]
+        });
+    }
+    
+    async run (client: Bot, interaction: CommandInteraction) {
 
         const input = interaction.options.get('userid');
         const userId = input?.value?.toString() || '';
@@ -35,5 +41,7 @@ export default new SlashCommand({
         interaction.reply({
             embeds: [embed]
         });
-    },
-});
+    }
+}
+
+export default new DropsCommand;
