45b36698adf648401bc47165810b6b956913c873
PADO <62028267+47PADO47@users.noreply.github.com>
2023-01-01 19:49:06 +0100

    ♻️ Refactor interactionCreate.ts event & add more interactions

--- src/events/interactionCreate.ts
+++ src/events/interactionCreate.ts
@@ -1,43 +1,61 @@
-import { CommandInteraction } from 'discord.js';
+import { BaseInteraction } from 'discord.js';
 import Bot from '../Bot';
 import Event from '../util/structures/Event';
+import Interaction from '../util/structures/Interaction';
+import SlashCommand from '../util/structures/SlashCommand';
 
-export default new Event({
-    name: 'interactionCreate',
-    run: async (client: Bot, interaction: CommandInteraction) => {
-        if (!interaction.isChatInputCommand()) return;
+class InteractionEvent extends Event {
+    constructor() {
+        super({
+            name: 'interactionCreate',
+        })
+    }
 
-        const slashCommand = client.slashCommands.get(interaction.commandName);
-        if (!slashCommand) return interaction.reply({ embeds: [
-            client.embed({
-                type: 'error',
-                text: 'Oops! Command does not exist!'
-            })
-        ], ephemeral: true });
-
-        if (slashCommand.category === 'splitgate' && !client.splitgate.authorized) return interaction.reply({ embeds: [
-            client.embed({
-                type: 'error',
-                text: 'Oops! The bot owner has not logged in on Splitgate yet!'
-            })
-        ], ephemeral: true });
-
-        if (slashCommand.category === 'owner' && !client.config.dev.ids.includes(interaction.user.id)) return interaction.reply({ embeds: [
-            client.embed({
-                type: 'error',
-                text: 'Oops! You are not the bot owner!'
-            })
-        ], ephemeral: true });
-
-        try {
-            await slashCommand.run(client, interaction);
-        } catch (error) {
-            if (!interaction.replied) interaction.reply({ embeds: [
-                client.embed({
+    private errorReply(client:Bot, message: string) {
+        return {
+            embeds: [client.embed({
                     type: 'error',
-                    text: `Oops! Something went wrong!\n\`\`\`js\n${error}\`\`\``
-                })
-            ], ephemeral: true });    
+                    text: message
+                })],
+            ephemeral: true, 
         }
     }
-});
+
+    private async executeInteraction(client:Bot, interaction: Interaction | SlashCommand, baseInteraction: any) {
+        try {
+            await interaction.run(client, baseInteraction);
+        } catch (error) {
+            if (!baseInteraction.replied) baseInteraction.reply(this.errorReply(client, `Oops! Something went wrong!\n\`\`\`js\n${error}\`\`\``));  
+        }
+    }
+    
+    async run(client: Bot, interaction: BaseInteraction) {
+        if (interaction.user.id === interaction.client.user?.id || interaction.user.bot) return;
+
+        if (interaction.isChatInputCommand()) {
+            const slashCommand = client.slashCommands.get(interaction.commandName);
+            if (!slashCommand) return interaction.reply(this.errorReply(client, 'Oops! Command does not exist!'));
+
+            if (slashCommand.category === 'splitgate' && !client.splitgate.authorized)
+             return interaction.reply(this.errorReply(client, 'Oops! The bot owner has not logged in on Splitgate yet!'));
+
+            if (slashCommand.category === 'owner' && !client.config.dev.ids.includes(interaction.user.id))
+             return interaction.reply(this.errorReply(client, 'Oops! You are not the bot owner!'));
+
+            return this.executeInteraction(client, slashCommand, interaction)
+        }
+
+        if (!interaction.isButton()) return;
+
+        let customInteraction = client.interactions.get(interaction.customId);
+        if (!customInteraction) customInteraction = client.interactions.find(button => interaction.customId.startsWith(button.customId));
+        if (!customInteraction) return interaction.reply(this.errorReply(client, 'Oops! Button does not exist!'));
+
+        if (customInteraction.authorOnly && interaction.user.id !== interaction.message.interaction?.user.id)
+         return interaction.reply(this.errorReply(client, 'Oops! You shall not click other user\'s buttons!'));
+
+        return this.executeInteraction(client, customInteraction, interaction)
+    }
+}
+
+export default new InteractionEvent;
