7335d78f201b688389cab520027cbd16a031bcb0
PADO <62028267+47PADO47@users.noreply.github.com>
2023-01-01 19:49:40 +0100

    ✨ Create interactions.ts handler

---
+++ src/handlers/interactions.ts
@@ -0,0 +1,20 @@
+import Bot from '../Bot';
+import path from 'path';
+import { readdirSync } from 'fs';
+import Interaction from '../util/structures/Interaction';
+
+async function handle(client: Bot): Promise<void> {
+    const interactionsPath = path.join(__dirname, '..', 'interactions',);
+    readdirSync(interactionsPath)
+    .forEach(interactionType => {
+        readdirSync(path.join(interactionsPath, interactionType))
+        .filter(file => file.endsWith('.js'))
+        .forEach(async (file) => {
+            const { default: interaction } = await import(path.join(interactionsPath, interactionType, file)) as { default: Interaction };
+            client.interactions.set(interaction.customId, interaction);
+            client.logger.info(`Loaded interaction ${interaction.customId} 🔤`);
+        });
+    });
+}
+
+export default handle;
