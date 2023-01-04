1ab9ec7a43ae98fbcd9a4f101ec547104b98529b
PADO <62028267+47PADO47@users.noreply.github.com>
2023-01-04 01:49:33 +0100

    ♻️ Allow more platforms in search.ts SlashCommand

--- src/commands/slash/splitgate/search.ts
+++ src/commands/slash/splitgate/search.ts
@@ -1,31 +1,103 @@
-import { EmbedBuilder } from 'discord.js';
+import { CommandInteraction, EmbedBuilder } from 'discord.js';
+import { platformId } from 'splitgate.js/dist/src/typings/v2';
+import Bot from '../../../Bot';
 import SlashCommand from '../../../util/structures/SlashCommand';
 
-export default new SlashCommand({
-    name: 'search',
-    description: 'Retrieve user id via user\'s steamId',
-    options: [
-        {
-            name: 'steamid',
-            description: 'The steam user identifier to search for',
-            type: 3,
-            required: true
-        }
-    ],
-    run: async (client, interaction) => {
+class SearchCommand extends SlashCommand {
+    constructor() {
+        super({
+            name: 'search',
+            description: 'Retrieve user id via user\'s steamId',
+            options: [
+                {
+                    name: 'profileid',
+                    description: 'The user identifier to search for (NOT GAMERTAG)',
+                    type: 3,
+                    required: true
+                },
+                {
+                    name: 'platform',
+                    description: 'User platform',
+                    type: 3,
+                    required: true,
+                    choices: [
+                        {
+                            name: 'Steam',
+                            value: 'steam'
+                        },
+                        {
+                            name: 'Xbox',
+                            value: 'live'
+                        },
+                        {
+                            name: 'Playstation',
+                            value: 'ps5'
+                        }
+                    ]
+                }
+            ]
+        });
+    }
 
-        const input = interaction.options.get('steamid');
-        const steamId = input?.value?.toString() || '';
+    async run (client: Bot, interaction: CommandInteraction) {
 
-        const data = await client.splitgate.getUserProfilesSteam([steamId]);
-        const info = data?.userIdPlatforms[0];
+        const userId = interaction.options.get('profileid')?.value?.toString()  || '';
+        const platform = (interaction.options.get('platform')?.value?.toString() || 'steam') as platformId;
+
+        const data = await client.splitgate.getPlatformUser(platform, userId);
+        const { codeBlock, getFormattedTimestamp } = client.utils;
+
         const embed = new EmbedBuilder()
         .setTitle('Search Results 🔎')
         .setColor('LuminousVividPink')
-        .setDescription(`**❯ UserId:** \`${info?.userId}\`\n**❯ Platform:** \`${info?.platformId}\``);
+        .setThumbnail(data.avatarUrl)
+        .setFields([
+            {
+                name: '**❯ Display name:**',
+                value: codeBlock(data.displayName),
+                inline: true
+            },
+            {
+                name: '**❯ Bans:**',
+                value: codeBlock(data.bans.length),
+                inline: true
+            },
+            {
+                name: '\u200b',
+                value: '\u200b',
+                inline: true,
+            },
+            {
+                name: '**❯ Country:**',
+                value: `${codeBlock(data.country)} :flag_${data.country.toLowerCase()}:`,
+                inline: true
+            },
+            {
+                name: '**❯ User id:**',
+                value: codeBlock(data.userId),
+                inline: true
+            },
+            {
+                name: '\u200b',
+                value: '\u200b',
+                inline: true,
+            },
+            {
+                name: '**❯ Email/Phone verified?**',
+                value: ['email', 'phone'].map(k => codeBlock(`• ${k}: ${data[k + 'Verified'] ? '☑️' : '❌'}`)).join('\n'),
+                inline: true
+            },
+            {
+                name: '**❯ Date of creation:**',
+                value: getFormattedTimestamp(data.createdAt),
+                inline: true
+            },
+        ])
         
         interaction.reply({
             embeds: [embed]
         })
-    },
-});
+    }
+}
+
+export default new SearchCommand;
