93628e1f5c63e0de11f4a3dd95383648b038a34c
PADO <62028267+47PADO47@users.noreply.github.com>
2023-01-04 01:51:26 +0100

    🩹 Remove once from warn.ts event

--- src/events/warn.ts
+++ src/events/warn.ts
@@ -1,10 +1,16 @@
 import Bot from '../Bot';
 import Event from '../util/structures/Event';
 
-export default new Event({
-    name: 'warn',
-    once: true,
-    run: async (client: Bot, warning: string) => {
+class WarnEvent extends Event {
+    constructor() {
+        super({
+            name: 'warn',
+        });
+    }
+
+    async run(client: Bot, warning: string) {
         client.logger.warn(warning);
     }
-});
+}
+
+export default new WarnEvent;
