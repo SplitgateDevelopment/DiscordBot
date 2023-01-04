dfc57b05c0f3aa448b48e674c2e50d8309ef746f
PADO <62028267+47PADO47@users.noreply.github.com>
2023-01-04 02:17:23 +0100

    🩹 Fix uptime commands

--- src/commands/message/misc/uptime.ts
+++ src/commands/message/misc/uptime.ts
@@ -1,19 +1,25 @@
-import { EmbedBuilder } from 'discord.js';
+import { Message, EmbedBuilder } from 'discord.js';
+import Bot from '../../../Bot';
 import Command from '../../../util/structures/Command';
 
-export default new Command({
-    name: 'uptime',
-    description: 'Retrieve the bot\'s uptime',
-    run: async (client, message) => {
-        
-        const { uptime, getFormattedTimestamp } = client.utils;
+class UptimeCommand extends Command {
+    constructor() {
+        super({
+            name: 'uptime',
+            description: 'Retrieve the bot\'s uptime',        
+        })
+    }
+
+    async run (client: Bot, message: Message) {
         const embed = new EmbedBuilder()
         .setTitle('Uptime 🔌')
-        .setDescription(`The bot is online since ${getFormattedTimestamp(uptime(), 'R')}>`)
+        .setDescription(`The bot is online since ${client.utils.getFormattedTimestamp(client.utils.uptime()*1000, 'R')}`)
         .setColor('Blue');
 
         message.reply({
             embeds: [embed]
         });
-    },
-});
+    }
+}
+
+export default new UptimeCommand;
--- src/commands/slash/misc/uptime.ts
+++ src/commands/slash/misc/uptime.ts
@@ -1,20 +1,26 @@
-import { EmbedBuilder } from 'discord.js';
+import { CommandInteraction, EmbedBuilder } from 'discord.js';
+import Bot from '../../../Bot';
 import SlashCommand from '../../../util/structures/SlashCommand';
 
-export default new SlashCommand({
-    name: 'uptime',
-    description: 'Retrieve the bot\'s uptime',
-    run: async (client, interaction) => {
-        
-        const { uptime, getFormattedTimestamp } = client.utils;
+class UptimeCommand extends SlashCommand {
+    constructor() {
+        super({
+            name: 'uptime',
+            description: 'Retrieve the bot\'s uptime',        
+        })
+    }
+
+    async run (client: Bot, interaction: CommandInteraction) {
         const embed = new EmbedBuilder()
         .setTitle('Uptime 🔌')
-        .setDescription(`The bot is online since ${getFormattedTimestamp(uptime(), 'R')}>`)
+        .setDescription(`The bot is online since ${client.utils.getFormattedTimestamp(client.utils.uptime()*1000, 'R')}`)
         .setColor('Blue');
 
         interaction.reply({
             embeds: [embed],
             ephemeral: true
         });
-    },
-});
+    }
+}
+
+export default new UptimeCommand;
