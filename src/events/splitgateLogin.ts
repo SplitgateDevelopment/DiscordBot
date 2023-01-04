1e0990b6e662426e4e3f0fc6a2899faa8866a4eb
PADO <62028267+47PADO47@users.noreply.github.com>
2023-01-04 20:02:42 +0100

    🔊 Update splitgateLogin.ts Event reward claiming log

--- src/events/splitgateLogin.ts
+++ src/events/splitgateLogin.ts
@@ -2,23 +2,32 @@ import { User } from 'splitgate.js/dist/src/typings/v2';
 import Bot from '../Bot';
 import Event from '../util/structures/Event';
 
-export default new Event({
-    name: 'splitgateLogin',
-    run: async (client: Bot, loginData: User) => {
+class SplitgateLoginEvent extends Event {
+    constructor() {
+        super({
+            name: 'splitgateLogin',
+        })
+    }
+    
+    async redeem(client: Bot) {
+        try {
+            const data = await client.splitgate.claimDailyReward();
+            client.logger.success(`Claimed daily reward, day ${data.dayOfWeek} (${data.daysClaimedCount}/7)`)
+        } catch (error) {
+            client.logger.error('Failed to claim daily reward')
+        }
+    }
+
+    async run (client: Bot, loginData: User) {
         client.logger.success(`Logged in on splitgate as ${loginData?.name || 'No name'} (${loginData.id || 'No id'})`);
 
         if (client.config.splitgate?.redeemDaily) {
-            redeem(client);
-            setInterval(() => redeem(client), 20*60*60*1000);
+            this.redeem(client);
+            setInterval(() => this.redeem(client), 20*60*60*1000);
         }
     }
-});
-
-async function redeem(client: Bot) {
-    try {
-        const data = await client.splitgate.claimDailyReward();
-        client.logger.success(`Claimed daily reward, day ${data.dayOfWeek}`)
-    } catch (error) {
-        client.logger.error('Failed to claim daily reward')
-    }
 }
+
+
+
+export default new SplitgateLoginEvent;
